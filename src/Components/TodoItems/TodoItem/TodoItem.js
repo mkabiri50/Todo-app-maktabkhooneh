952e28f6fe364a040bb8f54b3943a5ca96166c49
mahdi kabiri <mkabiri50@yahoo.com>
2019-08-01 00:01:28 +0430

    fixed todo

--- src/Components/TodoItems/TodoItem/TodoItem.js
+++ src/Components/TodoItems/TodoItem/TodoItem.js
@@ -13,39 +13,7 @@ class TOdoItem extends Component {
   
   }
       
-  handleSubmit= (event)=> {
-			const val = this.state.editText.trim();
-			if (val) {
-				this.props.onSave(val);
-				this.setState({editText: val});
-			} else {
-        this.handleDestroy()
-			}
-		}
-
-		handleEdit=() =>{
-			this.props.onEdit();
-			this.setState({editText: this.props.todo.title});
-		}
-
-		handleKeyDown= (event) =>{
-			if (event.which ===17) {
-				this.setState({editText: this.props.todo.title});
-				this.props.onCancel(event);
-			} else if (event.which === 13) {
-				this.handleSubmit(event);
-			}
-		}
-
-		handleChange=(event) =>{
-			if (this.props.editing) {
-				this.setState({editText: event.target.value});
-			}
-		}
-
-		getInitialState=() =>{
-			return {editText: this.props.todo.title};
-		}
+  
 
 
   render(){
@@ -53,18 +21,10 @@ class TOdoItem extends Component {
     return (
       <li className={todo.completed ?  'completed' : ''}>
         <div className='view' >
-          <input onChange={this.onToggle} type="checkbox" className='toggle' value = 'on' checked={todo.completed} />
-          <label onDoubleClick={this.handleEdit}>
-							{todo.title}
-						</label>
+          <input onChange={this.onToggle} type="checkbox" className='toggle' checked={todo.completed} />
+          <label >{todo.title}</label>
           <button onClick={this.handleDestroy} className='destroy'></button>
         </div>
-       <input 
-       className="edit"
-       value={this.state.editText}
-       onBlur={this.handleSubmit}
-       onChange={this.handleChange}
-       onKeyDown={this.handleKeyDown}/>
       </li>
       )
   }
